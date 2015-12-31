112294590bbd038f33cfb840c891c4d28a309b1a
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:12:12 +1300

    Converted to PSR-2

--- code/ping_config.php
+++ code/ping_config.php
@@ -3,17 +3,19 @@
 /*
 * Adds a setting field to the settings area of the CMS
 **/
-class PingConfig extends DataExtension {
+class PingConfig extends DataExtension
+{
  
     // Adds pingUrls to the database
-    public static $db = array(            
-        'pingUrls' => "TEXT",          
+    public static $db = array(
+        'pingUrls' => "TEXT",
     );
  
     /*
     * Updates the fields for the CMS
     **/
-    public function updateCMSFields(FieldList $fields) {
+    public function updateCMSFields(FieldList $fields)
+    {
         // Adds the field pingUrls to settings
         $fields->insertBefore(new Tab('Ping', 'Ping Settings'), 'Main');
         $fields->addFieldToTab("Root.Ping", new TextareaField("pingUrls", "Enter a list of URLs to ping. <br /><br />(Warning: adding a lot of URLs to this list will slow down saving of pages)"));
